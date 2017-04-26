eb9ac99ed9d3c22904e3a5bfe806392de0951566
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-04-26 15:19:28 +0200

    Added Question model

--- DjangoWebsite/Question/admin.py
+++ DjangoWebsite/Question/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Question.models import *
 
-# Register your models here.
+class QuestionDisplay(admin.ModelAdmin):
+    list_display = ('title', 'author', 'askDate', 'views', 'rate')
+    
+
+admin.site.register(Category)
+admin.site.register(Question, QuestionDisplay)
--- DjangoWebsite/Question/models.py
+++ DjangoWebsite/Question/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.SlugField('Name')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    def __unicode__(self):
+        return self.name
+
+class Question(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField('Title', max_length = 255)
+    askDate = models.DateTimeField('Ask Date', auto_now_add=True)
+    content = models.TextField('Content')
+    views = models.PositiveIntegerField('Views', default=0)
+    rate = models.IntegerField('Rates', default=0)
+    tags = models.ManyToManyField(Category, verbose_name = 'Tags')
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
+    def __unicode__(self):
+        return self.title
