14420e4ab76624bfc169e70f393f97302727617a
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-04-27 11:13:33 +0200

    Added css style to index header

--- DjangoWebsite/Question/views.py
+++ DjangoWebsite/Question/views.py
@@ -6,7 +6,7 @@ class IndexView(generic.ListView):
     template_name = 'index.html'
     context_object_name = 'latest_questions'
     model = models.Question
-    paginate_by = 10
+    paginate_by = 40
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
