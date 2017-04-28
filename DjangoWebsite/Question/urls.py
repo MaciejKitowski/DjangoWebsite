c6cebcb2e95b2f820191a122b8d4f01cc7bc7730
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-04-28 16:52:29 +0200

    Don't change sort tybe after page change

--- DjangoWebsite/Question/urls.py
+++ DjangoWebsite/Question/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from Question import views
 
 urlpatterns = [
-    url(r'^(?P<page>[\d]*)$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<page>[\d]*)/?$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<page>[\d]+)/(?P<sortby>[\w\-]+)$', views.IndexView.as_view(), name='index'),
 ]
