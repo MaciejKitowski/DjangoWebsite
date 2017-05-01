33dcabff569afede88f9f03773ff2c489a47d6a8
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-05-01 10:38:20 +0200

    Added answer count

--- DjangoWebsite/Question/views.py
+++ DjangoWebsite/Question/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.db.models import Count
 from Question import models
 
 class IndexView(generic.ListView):
@@ -10,12 +11,14 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         sortbyColumn = {'date-asc':'askDate', 'date-desc':'-askDate', 'votes-asc':'votes', 'votes-desc':'-votes', 'views-asc':'views', 'views-desc':'-views' }
-        
+        qr = super(IndexView, self).get_queryset()
+        qr = qr.annotate(answersCount=Count('answers'))
+
         if 'sortby' in self.kwargs and self.kwargs['sortby'] in sortbyColumn:
-            qr = super(IndexView, self).get_queryset().order_by(sortbyColumn[self.kwargs['sortby']])
+            qr = qr.order_by(sortbyColumn[self.kwargs['sortby']])
         else:
-            qr = super(IndexView, self).get_queryset().order_by(sortbyColumn['date-desc'])
-        
+            qr = qr.order_by(sortbyColumn['date-desc'])
+
         return qr
 
     def get_context_data(self, **kwargs):
