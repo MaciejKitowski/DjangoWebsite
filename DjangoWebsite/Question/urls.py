9c9d55da3a31993558d50d6c748e38bda22d1207
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-05-21 07:32:40 +0200

    Poprawki

--- DjangoWebsite/Question/urls.py
+++ DjangoWebsite/Question/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^(?P<page>[\d]+)/(?P<sortby>[\w\-]+)$', views.IndexView.as_view(), name='index'),
     url(r'^category/(?P<category>[\w\-\_]+)/(?P<page>[\d]+)/(?P<sortby>[\w\-]*)$', views.IndexView.as_view(), name='index'),
     url(r'^categories/(?P<page>[\d]*)', views.CategoriesView.as_view(), name='categories'),
-    url(r'^question/(?P<questionID>[\d]+})', views.QuestionView.as_view(), name='question'),
+    url(r'^question/(?P<questionID>[\d]+)', views.QuestionView.as_view(), name='question'),
     url(r'^question/add', views.NewQuestionView.as_view(), name='newQuestion'),
     url(r'^login/$', auth_view.login, {'template_name': 'login.html', 'authentication_form': forms.LoginForm}),
     url(r'^logout/$', auth_view.logout, kwargs={'next_page': '/'}),
