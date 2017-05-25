6872fb26c14cc284a16393a42474e9d54881f9ca
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-05-25 14:19:15 +0200

    Poprawki w importacg w urls.py

--- DjangoWebsite/Question/urls.py
+++ DjangoWebsite/Question/urls.py
@@ -1,20 +1,20 @@
+from Question import views as v
+from Question import forms as f
 from django.conf.urls import include, url
-from Question import views
-from django.contrib.auth import views as auth_view
-from Question import forms
+from django.contrib.auth import views as auth
 
 urlpatterns = [
-    url(r'^(?P<page>[\d]*)/?$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<page>[\d]+)/(?P<sortby>[\w\-]+)$', views.IndexView.as_view(), name='index'),
-    url(r'^category/(?P<category>[\w\-\_]+)/(?P<page>[\d]+)/(?P<sortby>[\w\-]*)$', views.IndexView.as_view(), name='index'),
-    url(r'^categories/(?P<page>[\d]*)', views.CategoriesView.as_view(), name='categories'),
-    url(r'^question/(?P<pk>[\d]+)', views.QuestionView.as_view(), name='question'),
-    url(r'^question/add', views.NewQuestionView.as_view(), name='newQuestion'),
-    url(r'^edit/answer/(?P<pk>[\d]+)', views.EditAnswerView.as_view(), name='edit_answer'),
-    url(r'^edit/question/(?P<pk>[\d]+)', views.EditQuestionView.as_view(), name='edit_question'),
-    url(r'^delete/answer/(?P<pk>[\d]+)', views.DeleteAnswerView.as_view(), name='delete_answer'),
-    url(r'^delete/question/(?P<pk>[\d]+)', views.DeleteQuestionView.as_view(), name='delete_question'),
-    url(r'^login/$', auth_view.login, {'template_name': 'auth/login.html', 'authentication_form': forms.LoginForm}),
-    url(r'^logout/$', auth_view.logout, kwargs={'next_page': '/'}),
-    url(r'^register/$', views.RegisterView.as_view()),
+    url(r'^(?P<page>[\d]*)/?$', v.IndexView.as_view(), name='index'),
+    url(r'^(?P<page>[\d]+)/(?P<sortby>[\w\-]+)$', v.IndexView.as_view(), name='index'),
+    url(r'^category/(?P<category>[\w\-\_]+)/(?P<page>[\d]+)/(?P<sortby>[\w\-]*)$', v.IndexView.as_view(), name='index'),
+    url(r'^categories/(?P<page>[\d]*)', v.CategoriesView.as_view(), name='categories'),
+    url(r'^question/(?P<pk>[\d]+)', v.QuestionView.as_view(), name='question'),
+    url(r'^question/add', v.NewQuestionView.as_view(), name='newQuestion'),
+    url(r'^edit/answer/(?P<pk>[\d]+)', v.EditAnswerView.as_view(), name='edit_answer'),
+    url(r'^edit/question/(?P<pk>[\d]+)', v.EditQuestionView.as_view(), name='edit_question'),
+    url(r'^delete/answer/(?P<pk>[\d]+)', v.DeleteAnswerView.as_view(), name='delete_answer'),
+    url(r'^delete/question/(?P<pk>[\d]+)', v.DeleteQuestionView.as_view(), name='delete_question'),
+    url(r'^login/$', auth.login, {'template_name': 'auth/login.html', 'authentication_form': f.LoginForm}),
+    url(r'^logout/$', auth.logout, kwargs={'next_page': '/'}),
+    url(r'^register/$', v.RegisterView.as_view()),
 ]
